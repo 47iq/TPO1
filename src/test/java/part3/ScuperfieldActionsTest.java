93fc99a8599a429c653382a725a6bf9ff7c57e5b
Pavel Danilov <pasha.danilov.perm@gmail.com>
2023-02-28 18:16:06 +0300

    Add actions test

---
+++ src/test/java/part3/ScuperfieldActionsTest.java
@@ -0,0 +1,61 @@
+package part3;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ScuperfieldActionsTest {
+    Actions scuperfieldActions = new ScuperfieldActions();
+
+    Station someStation = new SomeStation("TEST_STATION", 2);
+
+    Passenger randomPassenger = new SomePassenger("RANDOM", PassengerCondition.REGULAR_AWAKE,
+            someStation, 1, new ScuperfieldActions());
+
+    Passenger scuperfield = new SomePassenger("Scuperfield", PassengerCondition.REGULAR_AWAKE,
+            someStation, 1, new ScuperfieldActions());
+
+
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @Test
+    void testRandomPassengerScuperfieldActions() {
+        Exception exception = assertThrows(IllegalActionsTarget.class, () -> {
+            scuperfieldActions.completeActions(randomPassenger);
+        });
+    }
+
+    @Test
+    void testScuperfieldActions() throws UnableToFall {
+        scuperfieldActions.completeActions(scuperfield);
+        String output = """
+                //Previously Passenger Scuperfield lost his Cane which costs 10000.0.
+                Bottle tilts.
+                Soda spills from Bottle.
+                Bottle tilts.
+                Soda from Bottle spills on Passenger Scuperfield.
+                Passenger Scuperfield drinks sweet, great smelling, pleasantly pinching mouth Soda from Bottle.
+                Passenger Scuperfield thinks: "I would have to pay 10 for this Bottle"
+                Passenger Scuperfield thinks: "The sum of my loss has lowered. It's only 9990 now!"
+                Passenger Scuperfield is now satisfied.
+                Passenger Scuperfield thinks: "I have to take only 1000 train trips to get my money back!"
+                Passenger Scuperfield is now happy.
+                Bottle tilts.
+                Bottle falls on Passenger Scuperfield's head.
+                Bottle hits Passenger Scuperfield's forehead.
+                Passenger Scuperfield is now having a""";
+        String output1 = output + " small bump on the head.\n";
+        String output2 = output + " big bump on the head.\n";
+        Assertions.assertTrue(output1.equals(outputStreamCaptor.toString()) || output2.equals(outputStreamCaptor.toString()));
+    }
+}
