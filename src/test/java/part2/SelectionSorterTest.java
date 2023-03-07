98a15d638956e1fbc8fe07ec5c1ea871ba023444
Danila Lysenko <lysenko.d@profactory.io>
2023-03-07 15:10:44 +0300

    Parametrize tests for selection sort

--- src/test/java/part2/SelectionSorterTest.java
+++ src/test/java/part2/SelectionSorterTest.java
@@ -2,8 +2,12 @@ package part2;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,49 +15,80 @@ class SelectionSorterTest {
 
     SelectionSorter selectionSorter;
 
+    public static Stream<Arguments> sortRegularArray() {
+        return Stream.of(
+                Arguments.of(new int[] {10, 8, 4, 9, 6, 5, 3, 4, 1, 10}, new int[] {1, 3, 4, 4, 5, 6, 8, 9, 10, 10}),
+                Arguments.of(new int[] {3, 7, 2, 1, 0, 0, 1, 1, 7, 3}, new int[] {0, 0, 1, 1, 1, 2, 3, 3, 7, 7})
+        );
+    }
+
+    public static Stream<Arguments> sortAlreadySortedArray() {
+        return Stream.of(
+                Arguments.of(new int[] {1, 3, 4, 4, 5, 6, 8, 9, 10, 10}, new int[] {1, 3, 4, 4, 5, 6, 8, 9, 10, 10}),
+                Arguments.of(new int[] {0, 0, 1, 1, 1, 2, 3, 3, 7, 7}, new int[] {0, 0, 1, 1, 1, 2, 3, 3, 7, 7})
+        );
+    }
+
+    public static Stream<Arguments> sortSingleElement() {
+        return Stream.of(
+                Arguments.of(new int[] {1000}, new int[] {1000}),
+                Arguments.of(new int[] {Integer.MAX_VALUE}, new int[] {Integer.MAX_VALUE}),
+                Arguments.of(new int[] {Integer.MIN_VALUE}, new int[] {Integer.MIN_VALUE})
+        );
+    }
+
+    public static Stream<Arguments> sortNegativeAndPositiveArray() {
+        return Stream.of(
+                Arguments.of(new int[] {-8, -7, -9, -7, -4, 6, 5, -8, 4, -1}, new int[] {-9, -8, -8, -7, -7, -4, -1, 4, 5, 6}),
+                Arguments.of(new int[] {9, -1, 2, 6, -8, 4, 10, -5, -1, -1}, new int[] {-8, -5, -1, -1, -1, 2, 4, 6, 9, 10})
+        );
+    }
+
+    public static Stream<Arguments> sortNegativeArray() {
+        return Stream.of(
+                Arguments.of(new int[] {-2, -5, -3, -9, -1, -9, -3, -7, -7, -4}, new int[] {-9, -9, -7, -7, -5, -4, -3, -3, -2, -1}),
+                Arguments.of(new int[] {-3, -10, -1, -7, -8, -5, -2, -5, -7, -5}, new int[] {-10, -8, -7, -7, -5, -5, -5, -3, -2, -1})
+        );
+    }
+
     @BeforeEach
     public void setup() {
         selectionSorter = new SelectionSorter();
     }
 
-    @Test
-    void sortRegularArray() {
-        int[] sortArray = {10, 8, 4, 9, 6, 5, 3, 4, 1, 10};
-        int[] expectedArray = {1, 3, 4, 4, 5, 6, 8, 9, 10, 10};
-        selectionSorter.sort(sortArray);
-        assertArrayEquals(expectedArray, sortArray);
+    @ParameterizedTest
+    @MethodSource
+    void sortRegularArray(int[] input, int[] expected) {
+        selectionSorter.sort(input);
+        assertArrayEquals(input, expected);
     }
 
-    @Test
-    void sortAlreadySortedArray() {
-        int[] sortArray = {1, 3, 4, 4, 5, 6, 8, 9, 10, 10};
-        int[] expectedArray = {1, 3, 4, 4, 5, 6, 8, 9, 10, 10};
-        selectionSorter.sort(sortArray);
-        assertArrayEquals(expectedArray, sortArray);
+    @ParameterizedTest
+    @MethodSource
+    void sortAlreadySortedArray(int[] input, int[] expected) {
+        selectionSorter.sort(input);
+        assertArrayEquals(input, expected);
     }
 
-    @Test
-    void sortSingleElement() {
-        int[] sortArray = {1000};
-        int[] expectedArray = {1000};
-        selectionSorter.sort(sortArray);
-        assertArrayEquals(expectedArray, sortArray);
+    @ParameterizedTest
+    @MethodSource
+    void sortSingleElement(int[] input, int[] expected) {
+        selectionSorter.sort(input);
+        assertArrayEquals(input, expected);
     }
 
-    @Test
-    void sortNegativeAndPositiveArray() {
-        int[] sortArray = {-8, -7, -9, -7, -4, 6, 5, -8, 4, -1};
-        int[] expectedArray = {-9, -8, -8, -7, -7, -4, -1, 4, 5, 6};
-        selectionSorter.sort(sortArray);
-        assertArrayEquals(expectedArray, sortArray);
+    @ParameterizedTest
+    @MethodSource
+    void sortNegativeAndPositiveArray(int[] input, int[] expected) {
+        selectionSorter.sort(input);
+        assertArrayEquals(input, expected);
     }
 
-    @Test
-    void sortNegativeArray() {
-        int[] sortArray = {-2, -5, -3, -9, -1, -9, -3, -7, -7, -4};
-        int[] expectedArray = {-9, -9, -7, -7, -5, -4, -3, -3, -2, -1};
-        selectionSorter.sort(sortArray);
-        assertArrayEquals(expectedArray, sortArray);
+    @ParameterizedTest
+    @MethodSource
+    void sortNegativeArray(int[] input, int[] expected) {
+        selectionSorter.sort(input);
+        assertArrayEquals(input, expected);
     }
 
     @Test
